ab33000a32bea2408c1a4d2e120cbcdc2d335aac
Mathis Brossier <mathis.brossier@gmail.com>
2022-07-24 21:30:46 +0200

    show server closed dialog

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,20 @@
 import Svelte from "./ui/index.svelte"
 import { init as glInit } from './gl/global'
 
+import { pServer } from './ui/global'
+import { showError, showInfo } from './ui/lib/dialog'
+
 function main() {
   
   glInit()
+  
+  pServer.then(server => server.socket.onclose = async (e) => {
+    console.error(e)
+    await showError('Server closed: ' + (e.reason || 'unknown reason'))
+    const reload = await showInfo('Reload page?', 'yesno')
+    if (reload)
+      window.location.reload()
+  })
 
   new Svelte({
     target: document.body,
