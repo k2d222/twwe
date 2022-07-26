a789a352ef47e76c9e37e533679a5b6b144e80d9
Mathis Brossier <mathis.brossier@gmail.com>
2022-07-26 21:15:47 +0200

    blank env editor pane

---
+++ client/src/ui/lib/envelopeEditor.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   import type { RenderMap } from '../../gl/renderMap'
+
+   export let rmap: RenderMap
+   export let visible: boolean = false
+</script>
+
+<div id='envelope-editor' class:hidden={!visible}>
+   coucou
+</div>
