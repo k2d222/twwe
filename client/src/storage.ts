b9ab6223c0a280f8907ef896874c3c0605558f94
mathis <mathis.brossier@universite-paris-saclay.fr>
2023-10-15 12:22:08 +0200

    checked localstorage

--- client/src/storage.ts
+++ client/src/storage.ts
@@ -5,6 +5,7 @@ export interface ServerConfig {
   host: string
   port: number
   encrypted: boolean
+  path?: string
 }
 
 interface StorageSpec {
@@ -15,40 +16,61 @@ interface StorageSpec {
 interface StorageEntry<T> {
   clone: (inst: T) => T
   default: T
+  sanitize: (entry: any) => boolean
 }
 
 type StorageEntries = { [K in keyof StorageSpec]: StorageEntry<StorageSpec[K]> }
 
+function isServerConfig(entry: any): entry is ServerConfig {
+  return typeof entry === 'object' && ['name', 'host', 'port', 'encrypted'].every(k => entry.hasOwnProperty(k))
+}
+
 const entries: StorageEntries = {
   servers: {
-    clone: function (confs: ServerConfig[]) {
-      return confs.map(c => ({ ...c }))
-    },
+    clone: (confs: ServerConfig[]) => confs.map(c => ({ ...c })),
     default: VITE_SERVER_URLS
       .split(',')
       .map(url => url.split(':'))
       .map(([name, host, port, ssl]) => ({ name, host, port: parseInt(port), encrypted: ssl === '1' })),
+    sanitize: (entry: any) =>  Array.isArray(entry) && entry.every(e => isServerConfig(e))
   },
   currentServer: {
     clone: x => x,
     default: 0,
+    sanitize: (entry: any) => typeof entry === 'number' && entry < storage.load('servers').length
   },
 }
 
 const storage = {
   version: 3,
+  reset: function () {
+    localStorage.clear()
+    localStorage.setItem('version', '' + storage.version)
+    for (const [key, entry] of Object.entries(entries)) {
+      localStorage.setItem(key, JSON.stringify(entry.default))
+    }
+  },
   init: function () {
     const storedVersion = parseInt(localStorage.getItem('version') ?? '0')
     if (storedVersion !== storage.version) {
-      localStorage.clear()
-      localStorage.setItem('version', '' + storage.version)
-      for (const [key, entry] of Object.entries(entries)) {
-        localStorage.setItem(key, JSON.stringify(entry.default))
-      }
+      storage.reset()
     }
   },
   load: function <K extends keyof StorageSpec>(key: K): StorageSpec[K] {
-    return JSON.parse(localStorage.getItem(key) ?? '')
+    try {
+      const res = JSON.parse(localStorage.getItem(key))
+      if (entries[key].sanitize(res)) {
+        return res
+      }
+      else {
+        throw 'sanitization failed'
+      }
+    }
+    catch (e) {
+      console.error('localstorage failure:', e)
+      storage.reset()
+      return storage.load(key)
+    }
   },
 
   save: function <K extends keyof StorageSpec>(key: K, val: StorageSpec[K]) {
